5346d3838212b52fa100de8b0b4b2e5e16152e73
Philipe <philipe-leandro@hotmail.com>
2021-12-07 07:29:07 -0300

    Add files

---
+++ area_or_perimeter.rb
@@ -0,0 +1,7 @@
+def area_or_perimeter(l , w)
+  if l != w
+    2*l + 2*w
+  elsif l = w
+    l*w
+  end
+end
---
+++ simple_multiplication.rb
@@ -0,0 +1,7 @@
+def simple_multiplication(number)
+  if number.even? == true
+    number * 8
+  else
+    number * 9
+  end
+end
